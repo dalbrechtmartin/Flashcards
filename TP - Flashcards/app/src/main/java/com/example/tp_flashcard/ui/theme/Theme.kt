61405c0a0cfe8ae9e9e0d7c4d3b97691ae634cf8
Danaé A.M. <dev.danae@gmail.com>
2025-06-11 14:37:45 +0200

    CHG : Theme

--- TP - Flashcards/app/src/main/java/com/example/tp_flashcard/ui/theme/Theme.kt
+++ TP - Flashcards/app/src/main/java/com/example/tp_flashcard/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.tp_flashcard.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = White,
+    secondary = White,
+    tertiary = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Black,
+    secondary = Black,
+    tertiary = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,8 +35,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun TP_FlashcardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,7 +43,6 @@ fun TP_FlashcardTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
